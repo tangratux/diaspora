575fb689c984d2aa000ebc6d03a7573d627e9383
maxwell <maxwell@joindiaspora.com>
2010-10-15 23:23:17 -0700

    manage page now puts a fake flash, receive should also work

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -34,43 +34,35 @@ class RequestsController < ApplicationController
   def create
     aspect = current_user.aspect_by_id(params[:request][:aspect_id])
     account = params[:request][:destination_url].strip  
-    finger = EMWebfinger.new(account)
     
-    
-    finger.on_person{ |person|
-      
-      if person.class == "String"
-        env.['async.callback'].call [301, {'Content-Type' => 'text/plain', 'Location' => aspects_manage_path}, "bad!"]
+    EM::next_tick { 
+      finger = EMWebfinger.new(account)
+
+      finger.on_person{ |person|
+        rel_hash = {:friend => person}
+
+        Rails.logger.debug("Sending request: #{rel_hash}")
+
+        begin
+          @request = current_user.send_friend_request_to(rel_hash[:friend], aspect)
+          
+        rescue Exception => e
+          raise e unless e.message.include? "already"
+          flash[:notice] = I18n.t 'requests.create.already_friends', :destination_url => params[:request][:destination_url]
+        end
+
+        if @request
+          flash[:notice] =  I18n.t 'requests.create.success',:destination_url => @request.destination_url
+        else
+          flash[:error] = I18n.t 'requests.create.horribly_wrong'
+        end
       end
-
-      rel_hash = {:friend => person}
-
-      Rails.logger.debug("Sending request: #{rel_hash}")
-
-      begin
-        @request = current_user.send_friend_request_to(rel_hash[:friend], aspect)
-        respond = [301, {'Content-Type' => 'text/plain', 'Location' => aspects_manage_path}, respond_with @request]
-      rescue Exception => e
-        raise e unless e.message.include? "already"
-        flash[:notice] = I18n.t 'requests.create.already_friends', :destination_url => params[:request][:destination_url]
-        env["async.callback"].call respond
-        return
-      end
-
-      if @request
-        flash[:notice] =  I18n.t 'requests.create.success',:destination_url => @request.destination_url
-        env["async.callback"].call respond
-      else
-        flash[:error] = I18n.t 'requests.create.horribly_wrong'
-        env["async.callback"].call respond
-      end
-
-     env["async.callback"].call respond
     }
     
     finger.fetch
-
-    throw :async
+  }
+  flash[:notice] = "we tried our best to send a message to #{account}"
+  redirect_to aspects_manage_path
   end
 
 end
