e9919ef6be1a866380f5db13705b94a9d5e582f0
maxwell <maxwell@joindiaspora.com>
2010-10-16 13:04:55 -0700

    tests for WebfingerProfile

--- lib/em-webfinger.rb
+++ lib/em-webfinger.rb
@@ -1,7 +1,9 @@
 require File.join(Rails.root, 'lib/hcard')
+require File.join(Rails.root, 'lib/webfinger_profile')
 
 class EMWebfinger
   TIMEOUT = 5
+  
   def initialize(account)
     @account = account
     @callbacks = []
@@ -78,45 +80,3 @@ class EMWebfinger
 
 end
 
-class WebfingerProfile
-  attr_accessor :webfinger_profile, :account, :links, :hcard, :guid, :public_key, :seed_location
-  
-  def initialize(account, webfinger_profile)
-    @account = account
-    @webfinger_profile = webfinger_profile
-    @links = {}
-    set_fields
-  end
-  
-    def set_fields
-      doc = Nokogiri::XML.parse(webfinger_profile)
-
-      doc.css('Link').each do |l|  
-        rel = l.attribute("rel").value
-        href = l.attribute("href").value
-        @links[rel] = href
-        case rel
-          when "http://microformats.org/profile/hcard"
-            @hcard = href
-          when "http://joindiaspora.com/guid"
-            @guid = href     
-          when "http://joindiaspora.com/seed_location"
-            @seed_location = href
-        end
-      end
-
-      if doc.at('Link[rel=diaspora-public-key]')
-        begin
-          pubkey = doc.at('Link[rel=diaspora-public-key]').attribute('href').value 
-          @public_key = Base64.decode64 pubkey
-        rescue Exception => e
-          puts "probally not diaspora..."
-        end
-      end
-    end
-    
-    def valid_diaspora_profile?
-       !(@webfinger_profile.nil? || @account.nil? || @links.nil? || @hcard.nil? ||
-           @guid.nil? || @public_key.nil? || @seed_location.nil? )
-     end
-end
